0a1c1b7946d91b0285d8b625f807ef09b9ed2a53
{Reese <reese.mishler@ou.edu>
2020-11-09 16:19:07 -0600

    Added isEmpty method to check if cake is eaten

--- src/Cake.java
+++ src/Cake.java
@@ -1,4 +1,21 @@
 
 public class Cake {
+	private int bites = 10;
+	
+	
+	public Cake() {
+		// nothing to implement
+	}
+	
+	public void takeABite() {
+		--bites;
+		System.out.println("Yum!");
+	}
+	
+	public boolean isEmpty() {
+		return bites == 0;
+		// looks like an empty cake
+		
+	}
 
 }
